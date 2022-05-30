a54e5d0570a0db9533c7f699d79d8af162705f1f
Yong Jing <gohyongjing@gmail.com>
2022-05-30 23:44:44 +0800

    Add content to contact and menu page

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,27 @@
 function contact() {
     const content = document.querySelector("div#content");
     const container = document.createElement('div');
+    const h1 = document.createElement('h1');
+    const address = document.createElement('div');
+    const contact_number = document.createElement('div');
+    const email = document.createElement('div');
 
-    container.innerText = "contact us";
+    h1.innerText = "Contact us!"
+    
+    address.innerText = "Singapore Food Building #01-23 Food Street S123456";
+    address.classList.add("address");
+
+    contact_number.innerText = "1234 5678"
+    contact_number.classList.add("contact-number");
+
+    email.innerText = "yummyRestaurant@email.com"
+    email.classList.add("email");
+
+    container.classList.add("contact");
+    container.appendChild(h1);
+    container.appendChild(address);
+    container.appendChild(contact_number);
+    container.appendChild(email);
 
     content.appendChild(container);    
 }
--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,27 @@
+import Dish1Img from "./dish1.jpeg";
+import Dish2Img from "./dish2.jpeg";
+import Dish3Img from "./dish3.jpeg";
+
+
 function contact() {
     const content = document.querySelector("div#content");
     const container = document.createElement('div');
+    const dishImages = [Dish1Img, Dish2Img, Dish3Img];
 
-    container.innerText = "menu";
+    for (let i = 0; i < 3; i++) {
+        let foodItem = document.createElement('div');
+        let image = document.createElement('img');
+
+        foodItem.innerText = `Dish ${i + 1} $${i * 2 + 1}`;
+        foodItem.classList.add("dish");
+
+        image.src = dishImages[i];
+
+        foodItem.appendChild(image);
+        container.appendChild(foodItem);
+    }
+
+    container.classList.add("menu");
 
     content.appendChild(container);    
 }
